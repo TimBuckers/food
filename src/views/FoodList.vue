18ddaa10708f58bbd3547f592fc8d72ea7d3af9a
Ortix92 <nicktsutsunava@gmail.com>
2018-09-24 23:39:06 +0200

    List creation and user submission functional

--- src/views/FoodList.vue
+++ src/views/FoodList.vue
@@ -1,22 +1,81 @@
 <template>
     <v-container fluid>
         <v-slide-y-transition mode="out-in">
-            <v-layout column align-center>
-                {{participants}}
-            </v-layout>
+            <v-form ref="form" v-model="valid" lazy-validation>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-text-field v-model=name label="Name" :rules="[v => !!v || 'Name is required']" required>
+                        </v-text-field>
+                    </v-flex>
+                    <v-flex xs6>
+                        <v-checkbox v-model="veggy" label="Veggy"></v-checkbox>
+                    </v-flex>
+                    <v-flex xs6>
+                        <v-checkbox v-model="persistent" label="Full-time"></v-checkbox>
+                    </v-flex>
+                    <v-flex xs12>
+                        <v-text-field v-model=notes label="Notes">
+                        </v-text-field>
+                    </v-flex>
+                </v-layout>
+                <v-layout column align-center>
+                    <v-btn @click="submit">submit</v-btn>
+                </v-layout>
+            </v-form>
+
         </v-slide-y-transition>
+        <v-snackbar v-model=submitted>Added to list!</v-snackbar>
+
     </v-container>
 </template>
 
 <script>
 export default {
-  name: "FoodList",
-  props: ["participants"],
-  mounted() {},
-  methods: {},
-  data() {
-    return {};
-  }
+    name: "FoodList",
+    props: ["id"],
+    mounted() { },
+    data() {
+        return {
+            submitted: false,
+            valid: true,
+            list: [],
+            name: "",
+            veggy: false,
+            notes: "",
+            persistent: false,
+        };
+    },
+    methods: {
+        mutateUser(user) {
+            return this.$db.collection('users').doc(user.name.toLowerCase()).set(user)
+        },
+        submit() {
+            if (!this.$refs.form.validate()) {
+                return
+            }
+            let user = {
+                veggy: this.veggy,
+                name: this.name,
+                persistent: this.persistent
+            }
+
+            this.mutateUser(user).then(() => {
+                this.list.users.push(Object.assign({ notes: this.notes }, user))
+            }).then(() => {
+                // @todo: check if user already exists in this.lists.users
+                return this.$db.collection("lists")
+                    .doc(this.id)
+                    .set({ users: this.list.users }, { merge: true })
+            }).then(response => {
+                this.submitted = true
+            });
+        }
+    },
+    firestore() {
+        return {
+            list: this.$db.collection("lists").doc(this.id)
+        };
+    }
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,16 +8,17 @@
               <td>{{ props.item.type }}</td>
               <td>{{ toDate(props.item.created_at) }}</td>
               <td>{{ toDate(props.item.closed_at) }}</td>
-              <td>{{ props.item.users.length }}</td>
+              <td>{{ getUserAmount(props.item.users)}}</td>
               <td>{{ getVeggyAmount(props.item.users) }}</td>
             </tr>
           </template>
           <template slot="expand" slot-scope="props">
             <v-card flat>
-              <food-list :participants=props.item></food-list>
+              <food-list :id=props.item.id :listProp=props.item></food-list>
             </v-card>
           </template>
         </v-data-table>
+        <v-btn @click="newList">New</v-btn>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -28,22 +29,9 @@ import FoodList from "./FoodList.vue";
 export default {
   name: "Overview",
   components: { FoodList },
-  mounted() {},
-  methods: {
-    toDate(item) {
-      if (item == undefined) {
-        return "-";
-      }
-      return item.toDate().toLocaleTimeString("nl-NL");
-    },
-    getVeggyAmount(collection) {
-      return collection.filter(user => {
-        return user.veggy;
-      }).length;
-    }
-  },
   data() {
     return {
+      loading: true,
       headers: [
         { text: "Type", value: "type", sortable: false },
         { text: "Opened at", value: "created_at", sortable: false },
@@ -54,9 +42,40 @@ export default {
       lists: []
     };
   },
+  mounted() { },
+  methods: {
+    getUserAmount(users) {
+      return users.length;
+    },
+    newList() {
+      let list = {
+        created_at: new Date(),
+        users: [],
+        type: "lunch"
+      };
+      this.$db
+        .collection("lists")
+        .add(list)
+        .then(response => {
+          // @todo: automatically open new list
+        });
+    },
+    toDate(item) {
+      if (item == undefined) {
+        return "-";
+      }
+      return item.toDate().toLocaleTimeString("nl-NL");
+    },
+    getVeggyAmount(users) {
+      return users.filter(user => {
+        return user.veggy;
+      }).length;
+    }
+  },
+
   firestore() {
     return {
-      lists: this.$db.collection("lists")
+      lists: this.$db.collection("lists").orderBy("created_at", "desc")
     };
   }
 };
